d44f565fa46d500ab7f28a1e3344bd4e6f4bec57
Родион <glazun5602@gmail.com>
2021-07-21 16:04:07 +0300

    Вывел второй фильм

--- 2 lesson/03_favorite_movies.py
+++ 2 lesson/03_favorite_movies.py
@@ -9,6 +9,8 @@ first_film = my_favorite_movies[0:10]
 print('Первый фильм: ', first_film)
 last_film = my_favorite_movies[width-15: width]
 print('Последний фильм: ', last_film)
+second_film = my_favorite_movies[12:25]
+print('Второй фильм: ', second_film)
 
 
 # Выведите на консоль с помощью индексации строки, последовательно:
