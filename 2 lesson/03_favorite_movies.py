e8332c533d2e484668b3f3c310f129b2f0c585a0
Родион <glazun5602@gmail.com>
2021-07-21 16:06:33 +0300

    Вывел с конца второй фильм

--- 2 lesson/03_favorite_movies.py
+++ 2 lesson/03_favorite_movies.py
@@ -3,14 +3,7 @@
 
 # Есть строка с перечислением фильмов
 
-my_favorite_movies = 'Терминатор, Пятый элемент, Аватар, Чужие, Назад в будущее'
-width = len(my_favorite_movies)
-first_film = my_favorite_movies[0:10]
-print('Первый фильм: ', first_film)
-last_film = my_favorite_movies[width-15: width]
-print('Последний фильм: ', last_film)
-second_film = my_favorite_movies[12:25]
-print('Второй фильм: ', second_film)
+
 
 
 # Выведите на консоль с помощью индексации строки, последовательно:
@@ -22,4 +15,13 @@ print('Второй фильм: ', second_film)
 # Переопределять my_favorite_movies и использовать .split() нельзя.
 # Запятая не должна выводиться.
 
-# TODO здесь ваш код
+my_favorite_movies = 'Терминатор, Пятый элемент, Аватар, Чужие, Назад в будущее'
+width = len(my_favorite_movies)
+first_film = my_favorite_movies[0:10]
+print('Первый фильм: ', first_film)
+last_film = my_favorite_movies[width-15: width]
+print('Последний фильм: ', last_film)
+second_film = my_favorite_movies[12:25]
+print('Второй фильм: ', second_film)
+second_from_last_film = my_favorite_movies[width-22: width-17]
+print('Второй с конца фильм: ', second_from_last_film)
